8fac9c40001dfb3b1400ab54e2c59b681b9a3e48
Lucas Rosa <lucaslemosrosa@gmail.com>
2017-07-15 22:36:13 -0300

    Signal stuff

--- server/game.py
+++ server/game.py
@@ -1,4 +1,5 @@
 import constants
+import settings
 
 class Game:
     def __init__(self, players):
@@ -49,13 +50,31 @@ class Game:
             current_player = (current_player + 1) % len(players)
 
     def give_cards_and_two_coins(self):
-        pass
+        for player in players:
+            player.setAlive(True)
+            player.send_start([self.deck.draw_card(), self.deck.draw_card()])
+
+    def get_global_status(self):
+        player_list = []
+        for player in self.players:
+            player_dict = {"player": player.name(),
+                           "cards": player.get_visible_cards(),
+                           "coins": player.get_coins()
+                           }
+            player_list += [player_dict]
+        return {"players": player_list}
 
     def signal_status(self):
-        pass
+        for player in self.players:
+            if player.isAlive():
+                player.signal_status(self.get_global_status())
 
     def is_game_over(self):
-        pass
+        count = 0
+        for player in self.players:
+            if player.isAlive():
+                count += 1
+        return count == 1
 
     def signal_new_turn(self, player_index):
         pass
